ed610cc68c653d7535d6cf3764d51e52e35c9d05
tiagoarshavin <tiagoarshavin@hotmail.com>
2021-03-07 15:28:32 -0300

    criando cargo e vinculando com API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,67 @@
+
 import React, { useState } from 'react';
 import './App.css';
-import { Layout, Row, Col, Menu, Icon, Breadcrumb, Button, Table, Radio } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import Title from 'antd/lib/typography/Title';
-import SubMenu from 'antd/lib/menu/SubMenu';
+
+import Cargos from './components/Cargos';
 
 
 const { Header, Footer, Sider, Content } = Layout;
-function App() {
-	const dataSource = [];
 
-	const columns = [
-		{
-			title: 'Nome',
-			dataIndex: 'nome',
-			key: 'nome',
-		},
-	];
+function App() {
+
+	const [state, setState] = useState({
+		key: "cargos",
+		breadcrumb: "Cargos"
+	});
+
+	function menuSelecionado(menu) {
+		setState({ 
+			key: menu.key,
+			breadcrumb: menu.item.props.title 
+		})
+	}
+
+	function renderizarConteudo(key) {
+
+		switch (key) {
+			default:
+				return <Cargos />
+		}
+	}
 
 	return (
 		<div className="App">
 			<Layout>
 				<Header style={{ padding: 10 }}>
-					<Title style={{ color: 'white' }} level={3}>Desafio Prático</Title>
+					<Title style={{ color: 'white' }} level={3}>Desafio prático</Title>
 				</Header>
 				<Layout>
 					<Sider>
 						<Menu
-							defaultSelectedKeys={['Cargo']}
+							defaultSelectedKeys={['cargos']}
 							mode="inline"
+							onSelect={(menu) => menuSelecionado(menu)}
 						>
-							<Menu.Item key='Cargo'>
-								Cargo
-                           </Menu.Item>
+							<Menu.Item key='cargos' title="Cargos">
+								Cargos
+            				</Menu.Item>
 
-							<Menu.Item key='Perfil'>
-								Perfil
-                           </Menu.Item>
-
-							<Menu.Item key='Usuario'>
-								Usuário
-                           </Menu.Item>
 						</Menu>
 					</Sider>
-					<Content>
+					<Layout>
 						<Content style={{ padding: '0 25px' }}>
 							<Breadcrumb style={{ margin: '16px 0' }}>
-								<Breadcrumb.Item>Cargo</Breadcrumb.Item>
+								<Breadcrumb.Item>{state.breadcrumb}</Breadcrumb.Item>
 							</Breadcrumb>
 							<div style={{ background: '#fff', padding: 24, minHeight: 580 }}>
-								<Row>
-									<Col offset={21}>
-										<Button
-											icon={<PlusOutlined />}
-											type="primary"
-											size="middle"
-										>
-											Criar Cargo
-								</Button>
-									</Col>
-								</Row>
-
-								<br />
-								<Table
-									dataSource={dataSource}
-									columns={columns}
-								/>
+								{renderizarConteudo(state.key)}
 							</div>
 						</Content>
-					</Content>
+						<Footer style={{ textAlign: 'center' }}>Zetta@2021</Footer>
+					</Layout>
 				</Layout>
-				<Footer style={{ textAlign: 'center' }}>Zetta © 2021 </Footer>
 			</Layout>
 		</div>
 	);
